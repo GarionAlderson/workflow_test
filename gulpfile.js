b9d004b08fb37e08ac953c27b6385f8af38feb93
GarionAlderson <garrygooch@aol.com>
2018-12-14 08:29:17 +0000

    Added Browserify again

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
   coffee = require('gulp-coffee'),
-  browserify = require('gulp-browserify'),
+  compass = require('gulp-compass'),
   concat = require('gulp-concat');
+var browserify = require('browserify');
+var source     = require('vinyl-source-stream');
+
 
 var coffeeSources = (['components/coffee/tagline.coffee']);
 var jsSources = (['components/scripts/rclick.js',
 'components/scripts/pixgrid.js',
 'components/scripts/template.js',
 'components/scripts/tagline.js']);
+
+var sassSources = (['components/sass/style.scss']);
 
 gulp.task('coffee', function(done){
   gulp.src(coffeeSources)
@@ -23,5 +28,17 @@ gulp.task('js',function(done){
     .pipe(concat('script.js'))
       .pipe(browserify())
        .pipe(gulp.dest('builds/development/js'))
+  done();
+});
+
+gulp.task('compass', function(done){
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass:'components/sass',
+      image:'builds/development/images',
+      style:'expanded'
+    })
+      .on('error',gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
   done();
 });
